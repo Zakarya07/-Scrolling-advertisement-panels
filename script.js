7601745bd681be09d4a9602c530c2f4ca875659d
Zakarya <zakarya.elkhattabi@gmail.com>
2022-06-13 14:13:43 +0200

    Change the image of the Woman section && improve the animation of the cards (changing the duration and the animation-timing-function [ease-in], to make the animation transitate slowly at the beggining)

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 let cards=document.querySelectorAll(".card");
 
 window.addEventListener("load",()=>{ 
-
-for(let i =0; i< cards.length;i++){
-
-    let cardElement = cards[i];
-    setTimeout(() => {       
-    cardElement.style=`opacity:1;z-index:${i+1}`;
-}, 5000 * i);
-
-}
-
+    for(let i =0; i< cards.length;i++){
+        let cardElement = cards[i];
+        setTimeout(() => {       
+        cardElement.style=`opacity:1;z-index:${i+1}`;
+        }, 10000 * i);
+    }
 });
 
-
-let buyButtonForBgColoBouquet = document.querySelector(".bg_color_bouquet .text_container > .price_content > button");
-
-buyButtonForBgColoBouquet.addEventListener("click",()=>{
-        buyButtonForBgColoBouquet.innerHTML="redirecting...";
+let buyButtonForBgColorBouquet = document.querySelector(".bg_color_bouquet .text_container > .price_content > button");
+buyButtonForBgColorBouquet.addEventListener("click",()=>{
+        buyButtonForBgColorBouquet.innerHTML="redirecting...";
 })
